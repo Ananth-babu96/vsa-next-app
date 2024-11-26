f67df8b51d9ed17547eee1773fc7aeff8b2f9974
ananth <j.ananthrdsm@gmail.com>
2024-11-26 20:13:10 +0530

    added sitemap and robots.txt and gsc metatag

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,10 @@ export const metadata = {
    description:
       "VSA Creations, the top-rated photography studio in Kumbakonam, specializes in weddings, events, and baby shoots. Trust us to turn your memories into timeless art!",
    robots: "index,follow",
+   other: {
+      "google-site-verification": "xccayAt77bVxMDUFl_B-GKQNZVXV_3ebGCznao5ZAnY",
+   },
 };
-
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
